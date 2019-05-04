fd0a7d47ed59a7fda3d051914d66d6f4c3c9417a
Maksim Hryhoryeu <wolthrax@gmail.com>
2019-05-04 23:43:24 +0300

    add logger configuration

--- realty-web/src/main/java/by/home/hryhoryeu/realty/web/controllers/UserController.java
+++ realty-web/src/main/java/by/home/hryhoryeu/realty/web/controllers/UserController.java
@@ -1,17 +1,32 @@
 package by.home.hryhoryeu.realty.web.controllers;
 
+import by.home.hryhoryeu.realty.entities.model.role.Role;
+import by.home.hryhoryeu.realty.services.role.IRoleService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 @RequestMapping("/")
 public class UserController {
 
+    @Autowired
+    private IRoleService roleService;
+    private static final Logger LOG = LogManager.getLogger(UserController.class);
+
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView registrationUser(Model model) {
+
+        Role role = roleService.findByName("ROLE_ADMIN");
+        LOG.info(role.getId() + " : " + role.getValue());
+
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("definition-main-page");
         return modelAndView;
